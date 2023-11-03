c6de40744607e7c3e5cec8eb0ab06d75393fffc2
matejjakopec <jakopecmatej44@gmail.com>
2023-11-03 17:19:28 +0100

    cherry-pick

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Pozdrav iz aplikacije od Matej Jakopec e2b7791c42ac72afa6fcd23cc1af7b4a95afecfa");
+   printf("Pozdrav iz aplikacije od Matej Jakopec");
    return 0;
 }
